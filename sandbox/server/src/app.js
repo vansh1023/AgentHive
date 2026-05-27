5dac1926643b768044e808f6c27f7896e98db2e7
Vansh Choudhary <vanshchaudhary1023@gmail.com>
2026-05-27 16:03:53 +0530

    feat: React vite server pods and service functionality created

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import morgan from 'morgan'
+import { createPod } from './kubernetes/pod.js';
+import { createService } from './kubernetes/service.js';
+import { v7 as uuid } from "uuid"
 
 
 const app = express()
@@ -12,9 +15,28 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get('/api/sandbox/health', (req, res) => {
     res.status(200).json({
-        message: 'Sandbox API is healthy',
+        message: 'Sandbox API2 is healthy',
         status: 'Ok'
     })
+})
+
+
+
+app.post("/api/sandbox/start", async (req, res) => {
+
+    const sandboxId = uuid();
+
+    await Promise.all([
+        createPod(sandboxId),
+        createService(sandboxId)
+    ]);
+
+    return res.status(201).json({
+        message: 'Sandbox environment created successfully',
+        sandboxId,
+        previewUrl: `http://${sandboxId}.preview.localhost`
+    })
+
 })
 
 
